aeb1991dd41b0925408fee4fa1cf04430a2c1e08
ChoiRavi <RaviVoid@gmail.com>
2022-01-20 15:41:29 +0900

    셧다운

---
+++ src/Test_Shutdown.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Shutdown 명령수행
+ * @author smart04
+ *
+ */
+public class Test_Shutdown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			Process process = Runtime.exec("cmd.exe");
+			//cmd에 마우스 오버 후 Try/Catch선택
+			
+			OutputStream os = process.getOutputStream();
+			os.write("shutdown /s /f /t 90 \n\r".getBytes());
+			///s 컴퓨터를 종료합니다 
+			///f 사용자에게 미리 경고하지 않고 응용프로그램을 종료합니다
+			///t 종료전 시간제한 기간을 xxx초로 설정합니다.
+			os.close();
+			process.waitFor();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
