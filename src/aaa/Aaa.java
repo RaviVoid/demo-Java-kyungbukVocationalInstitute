2abdda892410ea06d622fc3510d9503b27c1753e
ravivoid <ravivoid@gmail.com>
2022-02-11 15:29:53 +0900

    aaa

---
+++ src/aaa/Aaa.java
@@ -0,0 +1,38 @@
+package aaa;
+/**
+ * 자바독 만들기 예제 클래스
+ * @author smart9
+ *
+ */
+public class Aaa {
+
+	/**
+	 * 필드
+	 * 
+	 */
+	private int i;
+	
+	
+	/**
+	 * 디폴트생성자
+	 */
+	public Aaa() {
+	}
+
+	/**
+	 * 기본 메소드
+	 */
+	public void method1() {
+		
+	}
+	
+	/**
+	 * method2 입니다.
+	 * @param i
+	 * @return
+	 */
+	public int method2(int i) {
+		return i;
+		
+	}
+}
